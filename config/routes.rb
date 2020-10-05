de8801ab854abc286c46990078bcc712feba3267
f81-kawano <f81.kawano.keiya@gmail.com>
2020-10-06 07:08:07 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   # get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
